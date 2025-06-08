e947f8760f0ced23fbe046f2e17376fc85fc9698
Cenk Şahin <cenk.sahin@hotmail.com.tr>
2025-06-08 12:25:48 +0300

    güncelleme

---
+++ UdemyAuthServer.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UdemyAuthServer.Core.Models;
+
+
+namespace UdemyAuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshToken { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //bulunduğu hiyerarşideki Configuration dosyalarını set edicek.
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ UdemyAuthServer.Service/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Models;
+
+
+namespace UdemyAuthServer.Service
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
